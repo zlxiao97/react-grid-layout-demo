18175a2f9f3c45384289ce99110d6bf9e8f1abdf
TadXiao <zlxiao97@yahoo.com>
2022-04-28 18:29:48 +0800

    响应式、可拖拽、可调节大小

--- src/App.js
+++ src/App.js
@@ -1,5 +1,73 @@
+import PageLayout from "./PageLayout";
 import "./styles.css";
+import "react-grid-layout/css/styles.css";
+import "react-resizable/css/styles.css";
+const layouts = {
+  xxl: [
+    { i: "l1", x: 1, y: 0, w: 3, h: 2, resizeHandles: ["e"] },
+    { i: "m1", x: 4, y: 0, w: 4, h: 2 },
+    { i: "r1", x: 8, y: 0, w: 3, h: 2 },
+    {
+      i: "l2",
+      x: 1,
+      y: 3, //即使大于2，也会向上吸附
+      w: 5,
+      h: 3
+    },
+    { i: "r2", x: 6, y: 2, w: 5, h: 3 }
+  ],
+  xl: [
+    { i: "l1", x: 1, y: 0, w: 2, h: 2, resizeHandles: ["e"] },
+    { i: "m1", x: 3, y: 0, w: 4, h: 2 },
+    { i: "r1", x: 7, y: 0, w: 2, h: 2 },
+    { i: "l2", x: 1, y: 2, w: 4, h: 3 },
+    { i: "r2", x: 5, y: 2, w: 4, h: 3 }
+  ],
+  lg: [
+    { i: "l1", x: 1, y: 0, w: 2, h: 2, resizeHandles: ["e"] },
+    { i: "m1", x: 3, y: 0, w: 2, h: 2 },
+    { i: "r1", x: 5, y: 0, w: 2, h: 2 },
+    { i: "l2", x: 1, y: 2, w: 3, h: 3 },
+    { i: "r2", x: 4, y: 2, w: 3, h: 3 }
+  ],
+  md: [
+    { i: "l1", x: 0, y: 0, w: 2, h: 2, resizeHandles: ["e"] },
+    { i: "m1", x: 2, y: 0, w: 2, h: 2 },
+    { i: "r1", x: 4, y: 0, w: 2, h: 2 },
+    { i: "l2", x: 0, y: 2, w: 3, h: 3 },
+    { i: "r2", x: 3, y: 2, w: 3, h: 3 }
+  ],
+  sm: [
+    { i: "l1", x: 0, y: 0, w: 1, h: 2, resizeHandles: ["e"] },
+    { i: "m1", x: 1, y: 0, w: 2, h: 2 },
+    { i: "r1", x: 3, y: 0, w: 1, h: 2 },
+    { i: "l2", x: 0, y: 2, w: 2, h: 3 },
+    { i: "r2", x: 2, y: 2, w: 2, h: 3 }
+  ],
+  xs: [
+    {
+      i: "l1",
+      x: 0,
+      y: 0,
+      w: 2,
+      h: 2,
+      resizeHandles: ["s"] // 利用断点可以配置个性化的
+    },
+    { i: "m1", x: 0, y: 2, w: 2, h: 2 },
+    { i: "r1", x: 0, y: 4, w: 2, h: 2 },
+    { i: "l2", x: 0, y: 7, w: 2, h: 3 },
+    { i: "r2", x: 0, y: 10, w: 2, h: 3 }
+  ]
+};
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <PageLayout layouts={layouts}>
+      <div key="l1">l1</div>
+      <div key="m1">m1</div>
+      <div key="r1">r1</div>
+      <div key="l2">l2</div>
+      <div key="r2">r2</div>
+    </PageLayout>
+  );
 }
---
+++ src/PageLayout.js
@@ -0,0 +1,21 @@
+import { Responsive, WidthProvider } from "react-grid-layout";
+
+const ResponsiveGridLayout = WidthProvider(Responsive);
+
+export default (props) => {
+  const defaultProps = {
+    className: "dev-helper",
+    isDraggable: true,
+    isResizable: true,
+    breakpoints: { xxl: 1600, xl: 1200, lg: 992, md: 768, sm: 576, xs: 480 },
+    cols: { xxl: 12, xl: 10, lg: 8, md: 6, sm: 4, xs: 2 },
+    rowHeight: 100, // 比较符合直觉
+    margin: [0, 0],
+    containerPadding: [0, 0]
+  };
+  return (
+    <ResponsiveGridLayout {...defaultProps} {...props}>
+      {props.children}
+    </ResponsiveGridLayout>
+  );
+};
